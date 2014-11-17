6ddbfd7aed5def9c4db0e4cad9efcd9cb1f50ec7
Ricky Dunlop <hello@rickydunlop.co.uk>
2014-11-17 12:21:38 +0000

    Removed cms related code

--- publisher/models.py
+++ publisher/models.py
@@ -1,6 +1,5 @@
 from django.utils import timezone
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist
 
 from .managers import PublisherManager, PublisherQuerySet
 from .utils import assert_draft
@@ -68,12 +67,6 @@ class PublisherModelBase(models.Model):
         if self.publisher_modified_at > self.publisher_linked.publisher_modified_at:
             return True
 
-        # Get all placeholders + their plugins to find their modified date
-        for placeholder_field in self.get_placeholder_fields():
-            placeholder = getattr(self, placeholder_field)
-            for plugin in placeholder.get_plugins_list():
-                if plugin.changed_date > self.publisher_linked.publisher_modified_at:
-                    return True
 
         return False
 
@@ -92,15 +85,6 @@ class PublisherModelBase(models.Model):
         if not draft_obj.publisher_linked:
             draft_obj.publisher_published_at = timezone.now()
 
-        if draft_obj.publisher_linked:
-            # Duplicate placeholder patch to prevent plugins from being deleted
-            # In some random cases a placeholder has been shared between the draft and published
-            # version of the page
-            self.patch_placeholders(draft_obj)
-
-            # Remove the current published record
-            draft_obj.publisher_linked.delete()
-
         # Duplicate the draft object and set to published
         publish_obj = self.__class__.objects.get(pk=self.pk)
         for fld in self.publisher_publish_empty_fields:
@@ -109,14 +93,10 @@ class PublisherModelBase(models.Model):
         publish_obj.publisher_published_at = draft_obj.publisher_published_at
 
         # Link the published obj to the draft version
-        # publish_obj.publisher_linked = draft_obj
         publish_obj.save()
 
         # Check for translations, if so duplicate the object
         self.clone_translations(draft_obj, publish_obj)
-
-        # Clone any placeholder fields into the new published object
-        self.clone_placeholder(draft_obj, publish_obj)
 
         # Clone relationships
         self.clone_relations(draft_obj, publish_obj)
@@ -129,23 +109,6 @@ class PublisherModelBase(models.Model):
         draft_obj.save(suppress_modified=True)
 
         publisher_post_publish.send(sender=draft_obj.__class__, instance=draft_obj)
-
-    @assert_draft
-    def patch_placeholders(self, draft_obj):
-        try:
-            from cms.utils.copy_plugins import copy_plugins_to  # noqa
-        except ImportError:
-            return
-
-        published_obj = draft_obj.publisher_linked
-
-        for field in self.get_placeholder_fields(draft_obj):
-            draft_placeholder = getattr(draft_obj, field)
-            published_placeholder = getattr(published_obj, field)
-
-            if draft_placeholder.pk == published_placeholder.pk:
-                published_placeholder.pk = None
-                published_placeholder.save()
 
     @assert_draft
     def unpublish(self):
@@ -206,57 +169,13 @@ class PublisherModelBase(models.Model):
                 translation.master = dst_obj
                 translation.save()
 
-    def clone_placeholder(self, src_obj, dst_obj):
-        try:
-            from cms.utils.copy_plugins import copy_plugins_to
-        except ImportError:
-            return
-
-        for field in self.get_placeholder_fields(src_obj):
-            src_placeholder = getattr(src_obj, field)
-            dst_placeholder = getattr(dst_obj, field)
-
-            dst_placeholder.pk = None
-            dst_placeholder.save()
-
-            setattr(dst_obj, field, dst_placeholder)
-            dst_obj.save()
-
-            src_plugins = src_placeholder.get_plugins_list()
-
-            # CMS automatically generates a new Placeholder ID
-            copy_plugins_to(src_plugins, dst_placeholder)
-
     def clone_relations(self, src_obj, dst_obj):
         """
         Since copying relations is so complex, leave this to the implementing class
         """
         pass
 
-    def get_placeholder_fields(self, obj=None):
-        placeholder_fields = []
 
-        try:
-            from cms.models.placeholdermodel import Placeholder
-        except ImportError:
-            return placeholder_fields
-
-        if obj is None:
-            obj = self
-
-        model_fields = obj.__class__._meta.get_all_field_names()
-        for field in model_fields:
-            if field in self.publisher_ignore_fields:
-                continue
-
-            try:
-                placeholder = getattr(obj, field)
-                if isinstance(placeholder, Placeholder):
-                    placeholder_fields.append(field)
-            except (ObjectDoesNotExist, AttributeError):
-                continue
-
-        return placeholder_fields
 
     def update_modified_at(self):
         self.publisher_modified_at = timezone.now()
